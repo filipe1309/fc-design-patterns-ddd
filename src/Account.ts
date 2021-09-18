27537a025ee94d31f31e34b0ddd7dd3daffda01b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-09-18 00:10:02 -0300

    feat: add transaction as an account aggregation

--- src/Account.ts
+++ src/Account.ts
@@ -1,29 +1,42 @@
 import AccountBuilder from "./AccountBuilder";
+import Transaction from "./Transaction";
 
 export default class Account {
     private bank: string | undefined;
     private branch: string | undefined;
     private account: string | undefined;
     private document: string;
-    private balance: number;
+    private transactions: Transaction[];
 
     constructor (accountBuilder: AccountBuilder) {
         this.bank = accountBuilder.bank;
         this.branch = accountBuilder.branch;
         this.account = accountBuilder.account;
         this.document = accountBuilder.document;
-        this.balance = 0;
+        this.transactions = [];
     }
 
     credit(amount: number) {
-        this.balance += amount;
+        this.transactions.push(new Transaction("credit", amount));
     }
 
     debit(amount: number) {
-        this.balance -= amount;
+        this.transactions.push(new Transaction("debit", amount));
     }
 
     getBalance() {
-        return this.balance;
+        let balance = 0;
+
+        for(const transaction of this.transactions) {
+            if (transaction.type == "credit") {
+                balance += transaction.amount;
+            }
+
+            if (transaction.type == "debit") {
+                balance -= transaction.amount;
+            }
+        }
+
+        return balance;
     }
 }
